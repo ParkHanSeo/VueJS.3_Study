bb870e707f68805249c48533096590c3b36c1c4c
ParkJeeHoon <gkstj8300@naver.com>
2022-11-14 17:30:46 +0900

    VueJS 스터디 진행중

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+// 라우터에 컴포넌틑 연결 3가지 방식
+// Import,  {}, webpackPrefetch 사용/미사용
 const routes = [
   {
     path: '/',
@@ -13,7 +14,32 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding/string',
+    name: 'DataBindingStringView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingStringView.vue'
+      )
+  },
+  {
+    path: '/databinding/html',
+    name: 'DataBindingHtmlView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingHtmlView.vue'
+      )
+  },
+  {
+    path: '/databinding/input',
+    name: 'DataBindingInputView',
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingInputView.vue'
+      )
   }
 ]
 
---
+++ src/views/1_databinding/DataBindingInputView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-model="userId" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userId: 'parkJee'
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+<style></style>
